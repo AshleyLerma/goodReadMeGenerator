d4aa2ada2375423a6211604d7ed17e486bfe4d00
Ashley Lerma <lerma.scout@gmail.com>
2020-06-20 15:36:56 -0500

    begining question setup

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-const questions = [];
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-function writeToFile(fileName, data) {}
+// const questions = [];
 
-function init() {}
+inquirer.prompt([
+  {
+    name: "username",
+    type: "input",
+    message: "GitHub Username:",
+    default: "Enter your username here",
+  },
+  {
+    name: "repoName",
+    type: "input",
+    message: "Repository Name:",
+    default: "Enter your repository name here",
+  },
+  {
+    name: "description",
+    type: "input",
+    message: "Description",
+    default: "Enter your repository description here",
+  },
+  {
+    name: "",
+    type: "input",
+    message: ":",
+    default: "",
+  },
+  {
+    name: "",
+    type: "input",
+    message: ":",
+    default: "",
+  },
+  {
+    name: "",
+    type: "input",
+    message: ":",
+    default: "",
+  },
+]);
 
-init();
+// function writeToFile(fileName, data) {}
+
+// function init() {}
+
+// init();
